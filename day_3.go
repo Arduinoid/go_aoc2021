504c1d1211af17367c80c1af46abc96c26a9f1b2
Jonathon Pinnow <pinnowj@gmail.com>
2021-12-07 23:14:16 -0500

    setup failing test

--- day_3.go
+++ day_3.go
@@ -1,3 +1,7 @@
 package main
 
 func dayThree() {}
+
+func powerConsumption(input []byte) int {
+	return 0
+}
--- day_3_test.go
+++ day_3_test.go
@@ -2,4 +2,23 @@ package main
 
 import "testing"
 
-func TestPowerConsumption(t *testing.T) {}
+func TestPowerConsumption(t *testing.T) {
+	// Arrange
+	expected := 90
+	input := []byte{
+		0b0100,
+		0b1101,
+		0b0101,
+		0b0010,
+		0b1100,
+	} //  0100
+
+	// Act
+	p := powerConsumption(input)
+
+	// Assert
+	if p != expected {
+		t.Logf("expected: %d, got: %d", expected, p)
+		t.Fail()
+	}
+}
